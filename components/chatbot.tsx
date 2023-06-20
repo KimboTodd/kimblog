e1d5e4b6c97f7247e2533852db92fe22cfcb67bc
KimboTodd <kimtoddsotheremail@gmail.com>
2023-06-20 18:20:54 -0400

    add prettier and tailwind extension

--- components/chatbot.tsx
+++ components/chatbot.tsx
@@ -60,14 +60,14 @@ const Chatbot = () => {
   };
 
   return (
-    <div className="rounded-2xl border-zinc-100  lg:border lg:p-6 md:border md:p-5 p-3 shadow-inner shadow-orange-600/20">
+    <div className="rounded-2xl border-zinc-100  p-3 shadow-inner shadow-orange-600/20 md:border md:p-5 lg:border lg:p-6">
       {messages.map(({ content, role }, index) => (
         <ChatLine key={index} role={role} content={content} />
       ))}
 
       {loading && <LoadingChatLine />}
 
-      <div className="mt-6 flex clear-both">
+      <div className="clear-both mt-6 flex">
         <input
           type="text"
           aria-label="chat input"
@@ -89,10 +89,10 @@ const Chatbot = () => {
         />
         <button
           type="submit"
-          className="ml-4 flex-none inline-flex items-center gap-2 justify-center 
-      rounded-md py-2 px-3 text-sm outline-offset-2 transition 
-      active:transition-none bg-zinc-600 font-semibold text-zinc-100
-      hover:bg-zinc-400 hover:ring-4 hover:ring-purple-500/10 active:bg-zinc-800 active:text-zinc-100/70"
+          className="ml-4 inline-flex flex-none items-center justify-center gap-2 
+      rounded-md bg-zinc-600 px-3 py-2 text-sm font-semibold 
+      text-zinc-100 outline-offset-2 transition hover:bg-zinc-400
+      hover:ring-4 hover:ring-purple-500/10 active:bg-zinc-800 active:text-zinc-100/70 active:transition-none"
           onClick={() => {
             sendMessage(input);
             setInput('');
