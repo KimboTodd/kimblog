58414438c7dea6bd9e82f76acce076a676a04777
KimboTodd <kimtoddsotheremail@gmail.com>
2023-08-02 22:07:20 -0400

    fix color of cells

--- components/tetris/cell.tsx
+++ components/tetris/cell.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { TETROMINOS } from './tetriminos';
+import { TETROMINOS } from './tetrominos';
 
 const Cell = ({ type }) => {
+  const color = TETROMINOS[type]?.color ?? '1,1,1';
+  console.log('Type and Color:', type, color);
   return (
     <div
       className={` aspect-square ${
-        type === 0 ? 'border-0' : 'border-4'
+        type === 'X' ? 'border-0' : 'border-4'
       } border-b-opacity-10 border-r-opacity-100 border-t-opacity-100 border-l-opacity-30 border-0`}
       style={{
-        borderBottomColor: `rgba(${TETROMINOS[type].color}, 0.1)`,
-        borderRightColor: `rgba(${TETROMINOS[type].color}, 1)`,
-        borderTopColor: `rgba(${TETROMINOS[type].color}, 1)`,
-        borderLeftColor: `rgba(${TETROMINOS[type].color}, 0.3)`,
-        backgroundColor: `rgba(${TETROMINOS[type].color}, 0.8)`,
+        borderBottomColor: `rgba(${color}, 0.1)`,
+        borderRightColor: `rgba(${color}, 1)`,
+        borderTopColor: `rgba(${color}, 1)`,
+        borderLeftColor: `rgba(${color}, 0.3)`,
+        backgroundColor: `rgba(${color}, 0.8)`,
       }}
     ></div>
   );
