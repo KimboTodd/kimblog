07b3fdc3fc374d58b5c9f52178581e9456b5e462
KimboTodd <kimtoddsotheremail@gmail.com>
2023-08-03 16:16:01 -0400

    pull collision into readable function variables to short circuit

--- lib/tetris/checkCollision.ts
+++ lib/tetris/checkCollision.ts
@@ -1,24 +1,28 @@
 import { CellState, Player, Position, Grid } from './types';
 
+const isFilledCell = (cell: string | number) => cell !== 0;
+
+// Check that the next row/col to move isn't undefined, if so this is out of bounds
+const isMoveOutOfBounds = (grid: Grid, nextY: number, nextX: number) =>
+  !grid[nextY] || !grid[nextY][nextX];
+
+const isMoveBlocked = (grid: Grid, nextY: number, nextX: number) =>
+  grid[nextY][nextX][1] === CellState.Merged;
+
 export const checkCollision = (
   player: Player,
   grid: Grid,
   { x: moveX, y: moveY }: Position
 ): boolean => {
   for (let y = 0; y < player.tetromino.length; y++) {
-    const row = player.tetromino[y];
-    for (let x = 0; x < row.length; x++) {
-      if (row[x] !== 0) {
-        const moveWithinBoardHeight = grid[y + player.pos.y + moveY];
-        const moveWithinBoardWidth =
-          grid[y + player.pos.y + moveY][x + player.pos.x + moveX];
-        const moveToMergedCell =
-          grid[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
-          CellState.Clear;
+    for (let x = 0; x < player.tetromino[y].length; x++) {
+      if (isFilledCell(player.tetromino[y][x])) {
+        const nextY = y + player.pos.y + moveY;
+        const nextX = x + player.pos.x + moveX;
+
         if (
-          !moveWithinBoardHeight ||
-          !moveWithinBoardWidth ||
-          moveToMergedCell
+          isMoveOutOfBounds(grid, nextY, nextX) ||
+          isMoveBlocked(grid, nextY, nextX)
         ) {
           return true;
         }
