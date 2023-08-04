8f9ef1abd5b926565d2454ceeda73e57c37e8fc3
KimboTodd <kimtoddsotheremail@gmail.com>
2023-08-04 18:50:57 -0400

    inline grid cols

--- components/dropchain/board.tsx
+++ components/dropchain/board.tsx
@@ -3,14 +3,12 @@ import Cell from './cell';
 import { Grid } from '../../lib/dropchain/types';
 
 const Board = ({ grid }: { grid: Grid }) => {
-  const GRID_COLS_TAILWIND = 'grid-cols-7';
-
   return (
     <div
-      className={`grid w-full max-w-md ${GRID_COLS_TAILWIND} gap-1 border-2 border-solid border-gray-700 bg-gray-900`}
+      className={`grid w-full max-w-md grid-cols-7 gap-1 border-2 border-solid border-gray-700 bg-gray-900`}
     >
       {grid.map(row =>
-        row.map((cell, x) => <Cell key={x} tetrominoName={cell[0]} />)
+        row.map((cell, x) => <Cell key={x} cell={cell} fill={cell[0]} />)
       )}
     </div>
   );
