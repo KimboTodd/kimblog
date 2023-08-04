a4e05ab8e8ac7fb17eae6b4f5a5af5527cba34eb
KimboTodd <kimtoddsotheremail@gmail.com>
2023-08-04 14:43:26 -0400

    ensure colors display per chain

--- components/dropchain/cell.tsx
+++ components/dropchain/cell.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { CHAINS } from './chains';
+import { CHAINS_ARRAY } from './chains';
 import { TetrominoName } from '../../lib/dropchain/types';
 
 const Cell = ({ tetrominoName }: { tetrominoName: TetrominoName | number }) => {
-  const color = CHAINS[tetrominoName]?.color ?? CHAINS[TetrominoName.X].color;
+  // To prevent tailwind tre-shaking these colors again, we must include them here
+  const CHAIN_COLORS = [
+    'text-yellow-300',
+    'text-green-300',
+    'text-blue-300',
+    'text-red-300',
+    'text-purple-300',
+    'text-orange-300',
+    'text-teal-300',
+  ];
+
+  const color: string =
+    CHAINS_ARRAY[tetrominoName]?.color ?? CHAINS_ARRAY[0].color;
   const content =
-    CHAINS[tetrominoName]?.shape[0][0] ?? CHAINS[TetrominoName.X].shape[0][0];
-  console.log('color', color);
-  console.log('tetrominoName', tetrominoName);
+    CHAINS_ARRAY[tetrominoName]?.shape[0][0] ?? CHAINS_ARRAY[0].shape[0][0];
   return (
     <div
-      className={`aspect-square ${color} ${
-        tetrominoName === TetrominoName.X
-          ? 'border-0'
-          : 'border-1 border-gray-800'
-      } border-b-opacity-10 border-r-opacity-100 border-t-opacity-100 border-l-opacity-30 border-0`}
+      className={`aspect-square ${color} border-2 border-gray-800 text-center align-middle text-lg`}
     >
       {content}
     </div>
