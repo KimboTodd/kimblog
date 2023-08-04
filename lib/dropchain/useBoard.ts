12fcfd857c333ec034ee19d6de1c0b4013741050
KimboTodd <kimtoddsotheremail@gmail.com>
2023-08-04 19:15:56 -0400

    start of scoring, probably needs a rethink

--- lib/dropchain/useBoard.ts
+++ lib/dropchain/useBoard.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { createGrid, newRow } from './grid';
+import { createGrid } from './grid';
 import { Cell, CellState, Player, Grid } from './types';
 import React from 'react';
 
@@ -7,36 +7,22 @@ export const useBoard = (
   player: Player,
   resetPlayer: () => void
 ): [Grid, React.Dispatch<React.SetStateAction<Grid>>, number] => {
-  const startingGrid: Grid = createGrid();
-  const [grid, setGrid] = useState<Grid>(startingGrid);
-  const [rowsCleared, setRowsCleared] = useState(0);
+  const initialGrid: Grid = createGrid();
+  const [grid, setGrid] = useState<Grid>(initialGrid);
+  const [chainsScored, setChainsScored] = useState(0);
 
   useEffect(() => {
-    setRowsCleared(0);
-
-    const sweepRows = (newGrid: Grid) =>
-      newGrid.reduce((acc: Grid, row: Cell[]) => {
-        // check if all cells within a row are filled
-        if (row.findIndex(cell => cell[0] === 0) === -1) {
-          setRowsCleared(prev => prev++);
-
-          // Add a new row to top
-          acc.unshift(newRow());
-          return acc;
-        }
-        // No filled row found, return this row
-        acc.push(row);
-        return acc;
-      }, []);
+    setChainsScored(0);
 
     const updatedGrid = (prevGrid: Grid) => {
-      // First clear the grid of any cells set to clear
+      // First clear the grid of any cells set to clear (due to movement)
       const newGrid: Grid = [];
       for (let y = 0; y < prevGrid.length; y++) {
         const newRow: Cell[] = [];
         for (let x = 0; x < prevGrid[y].length; x++) {
           const cell = prevGrid[y][x];
-          if (cell[1] === CellState.Clear) {
+          // TODO: probably want to handle clearing scored chains separately
+          if (cell[1] === CellState.Clear || cell[1] === CellState.Score) {
             newRow.push([0, CellState.Clear]);
           } else {
             newRow.push(cell);
@@ -45,24 +31,60 @@ export const useBoard = (
         newGrid.push(newRow);
       }
 
-      // Then draw new tetrominos
-      player.tetromino.forEach((row: (string | number)[], y: number) => {
-        row.forEach((fill: string | number, x: number) => {
-          if (fill !== 0) {
-            const cellState = player.collided
-              ? CellState.Merged
-              : CellState.Clear;
-            newGrid[y + player.pos.y][x + player.pos.x] = [
-              fill as number,
-              cellState,
-            ];
-          }
-        });
-      });
+      // Then draw new link in position
+      if (player.content !== 0) {
+        const cellState = player.collided ? CellState.Merged : CellState.Clear;
+        newGrid[player.pos.y][player.pos.x] = [player.content, cellState];
+      }
 
       if (player.collided) {
         resetPlayer();
-        return sweepRows(newGrid);
+        // handle chain clearing
+
+        // Row by row, handle clearing horizontal chains
+        var chainStart = null;
+        var chainEnd = null;
+        for (let y = 0; y < newGrid.length; y++) {
+          const row = newGrid[y];
+          for (let x = 0; x < row.length; x++) {
+            const fill = row[x][0];
+
+            // a filled cell is found
+            if (fill && fill !== 0) {
+              chainStart = chainStart === null ? x : chainStart;
+              chainEnd = x + 1;
+            }
+
+            // an empty cell is found, or we are at the last element in the row
+            if (!fill || fill === 0 || x === row.length - 1) {
+              if (chainStart === null) {
+                // if we have not started a chain, return early
+                continue;
+              }
+
+              const contiguousLength = chainEnd - chainStart;
+              for (let chainI = chainStart; chainI < chainEnd; chainI++) {
+                const contiguousElement = row[chainI];
+                if (contiguousElement[0] === contiguousLength) {
+                  // mark for scoring
+                  setChainsScored(prev => prev++);
+
+                  contiguousElement[1] = CellState.Score;
+                }
+              }
+
+              chainStart = null;
+              chainEnd = null;
+            }
+          }
+        }
+
+        // consider transposing and running this funciton again to handle vertical chains
+        // function transposeArray<T>(array: T[][]): T[][] {
+        //   return array[0].map((_, colIndex) => array.map(row => row[colIndex]));
+        // }
+
+        return newGrid;
       }
 
       return newGrid;
@@ -73,9 +95,9 @@ export const useBoard = (
     player.collided,
     player.pos.x,
     player.pos.y,
-    player.tetromino,
+    player.content,
     resetPlayer,
   ]);
 
-  return [grid, setGrid, rowsCleared];
+  return [grid, setGrid, chainsScored];
 };
