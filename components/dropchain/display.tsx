ee3c3f22052023d1701f009d91fbf1a34db52507
KimboTodd <kimtoddsotheremail@gmail.com>
2023-08-05 18:29:51 -0400

    update colors

--- components/dropchain/display.tsx
+++ components/dropchain/display.tsx
@@ -3,7 +3,7 @@ import React, { memo } from 'react';
 const Display = ({ gameOver, text }) => (
   <div
     className={`mb-6 box-border flex w-full border-4 border-solid border-gray-700 p-4 text-center 
-    ${gameOver ? 'text-red-500' : 'text-gray-600'} bg-black text-sm`}
+    ${gameOver ? 'text-red-500' : 'text-neutral-500'} bg-black text-sm`}
   >
     {text}
   </div>
--- components/dropchain/startButton.tsx
+++ components/dropchain/startButton.tsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 
 const StartButton = ({ callback }) => (
   <button
-    className="mb-6 box-border w-full cursor-pointer border-none bg-gray-700 p-4 text-lg text-gray-50 outline-none"
+    className="mb-6 box-border w-full cursor-pointer border-4 border-neutral-600 p-4 text-lg text-neutral-300 outline-none"
     onClick={callback}
   >
     Start Game
