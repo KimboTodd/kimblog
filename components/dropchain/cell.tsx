e1190760b54a5e54a106a9495ec4c6dac2512737
KimboTodd <kimtoddsotheremail@gmail.com>
2023-08-07 14:08:42 -0400

    cell animations when scoring handled by cell component

--- components/dropchain/cell.tsx
+++ components/dropchain/cell.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CHAINS_ARRAY } from './chains';
 import { CellState } from '../../lib/dropchain/types';
 
 const Cell = ({ fill, cell }) => {
-  // To prevent tailwind tre-shaking these colors again, we must include them here
+  const [animationTimeoutId, setAnimationTimeoutId] = useState(null);
+  const [animating, setAnimating] = useState(false);
+  const [cellState, setCellState] = useState({
+    state: cell[1],
+    content: CHAINS_ARRAY[fill].content ?? CHAINS_ARRAY[0].content,
+    color: CHAINS_ARRAY[fill]?.color ?? CHAINS_ARRAY[0].color,
+  });
+
+  // To prevent tailwind tree-shaking these colors again, we must include them here
+  // eslint-disable-next-line no-unused-vars
   const CHAIN_COLORS = [
     'text-yellow-400',
     'text-green-400',
@@ -15,18 +24,57 @@ const Cell = ({ fill, cell }) => {
     'animate-ping',
   ];
 
-  const color: string = CHAINS_ARRAY[fill]?.color ?? CHAINS_ARRAY[0].color;
-  const content = CHAINS_ARRAY[fill]?.content ?? CHAINS_ARRAY[0].content;
-  if (cell[1] === CellState.Score) {
-    console.log('cell score 🔥');
-  }
+  useEffect(() => {
+    if (animating) {
+      return;
+    }
+
+    if (cell[1] === CellState.Score) {
+      setCellState(prevState => ({
+        ...prevState,
+        content: CHAINS_ARRAY[fill]?.content,
+        state: CellState.Score,
+      }));
+      setAnimating(true);
+
+      clearTimeout(animationTimeoutId);
+      const newTimeoutId = setTimeout(() => {
+        setCellState(prevState => ({
+          ...prevState,
+          content: CHAINS_ARRAY[fill]?.content,
+          state: cell[1],
+          color: CHAINS_ARRAY[fill].color,
+        }));
+        setAnimating(false);
+      }, 500);
+      setAnimationTimeoutId(newTimeoutId);
+    } else if (
+      cellState.state === CellState.Score &&
+      cell[1] !== CellState.Score
+    ) {
+      setCellState(prevState => ({
+        ...prevState,
+        content: CHAINS_ARRAY[fill]?.content,
+        state: cell[1],
+        color: CHAINS_ARRAY[fill].color,
+      }));
+    } else {
+      setCellState(prevState => ({
+        ...prevState,
+        content: CHAINS_ARRAY[fill]?.content,
+        state: cell[1],
+        color: CHAINS_ARRAY[fill].color,
+      }));
+    }
+  }, [cell, animationTimeoutId, fill, animating, cellState.state]);
+
   return (
     <div
-      className={`aspect-square ${color} ${
-        cell[1] === CellState.Score ? 'animate-ping' : ''
+      className={`aspect-square ${cellState.color} ${
+        cellState.state === CellState.Score ? 'animate-ping' : ''
       } flex items-center justify-center border-2 border-gray-800 text-3xl`}
     >
-      {content}
+      {cellState.content}
     </div>
   );
 };
