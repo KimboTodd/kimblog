6a451d56050747e7c7a5affecc14ff45a65f0acf
KimboTodd <kimtoddsotheremail@gmail.com>
2023-08-07 16:59:53 -0400

    color buttons

--- components/dropchain/startButton.tsx
+++ components/dropchain/startButton.tsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 
 const StartButton = ({ callback }) => (
   <button
-    className="mb-6 box-border w-full cursor-pointer border-4 border-neutral-600 p-4 text-lg text-neutral-300 outline-none"
+    className="mb-6 box-border w-full cursor-pointer border-4 border-breige-500 p-4 text-lg text-breige-500 outline-none"
     onClick={callback}
   >
     Start Game
--- components/tetris/display.tsx
+++ components/tetris/display.tsx
@@ -2,8 +2,8 @@ import React, { memo } from 'react';
 
 const Display = ({ gameOver, text }) => (
   <div
-    className={`mb-6 box-border flex w-full border-4 border-solid border-gray-700 p-4 text-center 
-    ${gameOver ? 'text-red-500' : 'text-gray-600'} bg-black text-sm`}
+    className={`mb-6 box-border flex w-full border-4 border-solid border-breige-500 p-4 text-center 
+    ${gameOver ? 'text-red-500' : 'border-breige-500'} bg-slate-550 text-sm`}
   >
     {text}
   </div>
