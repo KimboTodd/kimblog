bef64c83089f7fc6a8d3d8a577e5f161a0e810c6
KimboTodd <kimtoddsotheremail@gmail.com>
2023-08-07 21:19:11 -0400

    display text no longer handles game over

--- components/dropchain/board.tsx
+++ components/dropchain/board.tsx
@@ -5,10 +5,14 @@ import { Grid } from '../../lib/dropchain/types';
 const Board = ({ grid }: { grid: Grid }) => {
   return (
     <div
-      className={`grid w-full max-w-md grid-cols-7 gap-1 border-2 border-solid border-gray-700 bg-gray-900`}
+      className={`mb-6 grid w-full max-w-md grid-cols-7 border-d border-2 border-b-4 border-green-600`}
     >
-      {grid.map(row =>
-        row.map((cell, x) => <Cell key={x} cell={cell} fill={cell[0]} />)
+      {grid.map((row, i) =>
+        row.map((cell, x) => {
+          return (
+            <Cell key={x} cell={cell} fill={cell[0]} stagingRow={i === 0} />
+          );
+        })
       )}
     </div>
   );
--- components/dropchain/display.tsx
+++ components/dropchain/display.tsx
@@ -1,9 +1,9 @@
 import React, { memo } from 'react';
 
-const Display = ({ gameOver, text }) => (
+const Display = ({ text }) => (
   <div
-    className={`mb-6 box-border flex w-full border-4 border-solid border-gray-700 p-4 text-center 
-    ${gameOver ? 'text-red-500' : 'text-neutral-500'} bg-black text-sm`}
+    className={`mb-6 box-border flex w-full animate-crtBlurText border-2 border-dashed 
+    border-green-700 p-2 text-center font-mono text-lg text-green-500`}
   >
     {text}
   </div>
